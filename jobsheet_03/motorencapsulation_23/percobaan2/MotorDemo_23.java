4d7ad32d8000b87253a1c4a381900d4bbf0685e9
RizkyaSalsabila <rzkysalsabila@gmail.com>
2024-09-22 11:36:58 +0700

    add percobaan 2

---
+++ jobsheet_03/motorencapsulation_23/percobaan2/MotorDemo_23.java
@@ -0,0 +1,23 @@
+public class MotorDemo_23 {
+    public static void main(String[] args) {
+        //objek
+        Motor_23 motor = new Motor_23();
+        motor.printStatus();
+        motor.tambahKecepatan();
+
+        motor.nyalakanMesin();
+        motor.printStatus();
+
+        motor.tambahKecepatan();
+        motor.printStatus();
+
+        motor.tambahKecepatan();
+        motor.printStatus();
+
+        motor.tambahKecepatan();
+        motor.printStatus();
+
+        motor.matikanMesin();
+        motor.printStatus();
+    }
+}
---
+++ jobsheet_03/motorencapsulation_23/percobaan2/Motor_23.java
@@ -0,0 +1,41 @@
+public class Motor_23 {
+    //atribut
+    private int kecepatan = 0;
+    private boolean kontakOn = false;
+
+    //method
+    public void nyalakanMesin() {
+        kontakOn = true;
+    }
+
+    public void matikanMesin() {
+        kontakOn = false;
+        kecepatan = 0;
+    }
+
+    public void tambahKecepatan() {
+        if (kontakOn == true) {
+            kecepatan += 5;
+        } else {
+            System.out.println("Kecepatan tidak bisa bertambah karena mesin off! \n");
+        }
+    }
+
+    public void kurangiKecepatan() {
+        if (kontakOn == true) {
+            kecepatan -= 5;
+        } else {
+            System.out.println("Kecepatan tidak bisa berkurang karena mesin off! \n");
+        }
+    }
+
+    public void printStatus() {
+        if (kontakOn == true) {
+            System.out.println("Kontak ON");
+        } else {
+            System.out.println("Kontak OFF");
+        }
+
+        System.out.println("Kecepatan " + kecepatan + "\n");
+    }
+}
