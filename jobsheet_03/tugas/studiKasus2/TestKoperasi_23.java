acbc8ed6b81bfc3bfcf79a032b063633912ca736
RizkyaSalsabila <rzkysalsabila@gmail.com>
2024-09-23 11:53:33 +0700

    modif number 5 to number 6

--- jobsheet_03/tugas/studiKasus2/TestKoperasi_23.java
+++ jobsheet_03/tugas/studiKasus2/TestKoperasi_23.java
@@ -15,49 +15,49 @@ public class TestKoperasi_23 {
         
         do {
             System.out.println("-------------------------------------");
-        System.out.println("--- TES KOPERASI ----");
-        System.out.println("-------------------------------------");
-        System.out.println("1. Pinjam Uang");
-        System.out.println("2. Bayar Angsuran");
-        System.out.println("3. Keluar");
-        System.out.print("\nPilih Menu (1/2/3) : ");
-        pilih = input23.nextInt();
-        input23.nextLine();
+            System.out.println("--- TES KOPERASI ----");
+            System.out.println("-------------------------------------");
+            System.out.println("1. Pinjam Uang");
+            System.out.println("2. Bayar Angsuran");
+            System.out.println("3. Keluar");
+            System.out.print("\nPilih Menu (1/2/3) : ");
+            pilih = input23.nextInt();
+            input23.nextLine();
 
-        switch (pilih) {
-            case 1:
-                System.out.println("-------------------------------------");
-                System.out.println("PEMINJAMAN UANG");
-                System.out.println("-------------------------------------");
-                System.out.print("Masukkan uang yang akan dipinjam : ");
-                int totalPinjam = input23.nextInt();
-                input23.nextLine();
-                donny.pinjam(totalPinjam);
-                break;
-            case 2:
-                System.out.println("-------------------------------------");
-                System.out.println("PEMBAYARAN ASURANSI");
-                System.out.println("-------------------------------------");
-                System.out.print("Masukkan asuransi peminjaman yang Anda bayar : ");
-                int bayarAsuransi = input23.nextInt();
-                input23.nextLine();
-                donny.angsur(bayarAsuransi);
-                break;
-            case 3:
-                System.out.println("-------------------------------------");
-                System.out.println("KELUAR");
-                System.out.println("-------------------------------------");
-                System.out.println("Terimakasihh...");
-                System.out.println("Anda telah keluar dari sistem.");
-                System.exit(0);
-                break;
-            default:
-                System.out.println("Menu yang Anda inputkan tidak ada");
-                System.out.println("Silahkan coba lagi !!");
-                break;
-            }
-        } while (pilih != 0);
-        
-            input23.close();
-        }
+            switch (pilih) {
+                case 1:
+                    System.out.println("-------------------------------------");
+                    System.out.println("PEMINJAMAN UANG");
+                    System.out.println("-------------------------------------");
+                    System.out.print("Masukkan uang yang akan dipinjam : ");
+                    int totalPinjam = input23.nextInt();
+                    input23.nextLine();
+                    donny.pinjam(totalPinjam);
+                    break;
+                case 2:
+                    System.out.println("-------------------------------------");
+                    System.out.println("PEMBAYARAN ASURANSI");
+                    System.out.println("-------------------------------------");
+                    System.out.print("Masukkan asuransi peminjaman yang Anda bayar : ");
+                    int bayarAsuransi = input23.nextInt();
+                    input23.nextLine();
+                    donny.angsur(bayarAsuransi);
+                    break;
+                case 3:
+                    System.out.println("-------------------------------------");
+                    System.out.println("KELUAR");
+                    System.out.println("-------------------------------------");
+                    System.out.println("Terimakasihh...");
+                    System.out.println("Anda telah keluar dari sistem.");
+                    System.exit(0);
+                    break;
+                default:
+                    System.out.println("Menu yang Anda inputkan tidak ada");
+                    System.out.println("Silahkan coba lagi !!");
+                    break;
+                }
+        } while (pilih != 0);   
+         
+    input23.close();
     }
+}
