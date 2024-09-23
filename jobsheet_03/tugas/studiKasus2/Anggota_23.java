86ceb1e5a1250019286b43e7a03c131efce548ac
RizkyaSalsabila <rzkysalsabila@gmail.com>
2024-09-23 10:50:39 +0700

    add number 4

---
+++ jobsheet_03/tugas/studiKasus2/Anggota_23.java
@@ -0,0 +1,47 @@
+package tugas.studiKasus2;
+
+public class Anggota_23 {
+    //atribut
+    private String nomorKTP, nama;
+    private int limitPeminjaman, jumlahPeminjaman;
+
+    //konstruktor
+    public Anggota_23(String noKTP, String nm, int limitPjmn) {
+        nomorKTP = noKTP;
+        nama = nm;
+        limitPeminjaman = limitPjmn;
+        this.jumlahPeminjaman = 0;
+    }
+
+    //method
+    //getter
+    public String getNama() {
+        return nama;
+    }
+
+    public int getLimitPinjaman() {
+        return limitPeminjaman;
+    }
+
+    public int getJumlahPinjaman() {
+        return jumlahPeminjaman;
+    }
+
+    public void pinjam(int uang) {
+        if ((jumlahPeminjaman + uang) > limitPeminjaman) {
+            System.out.println("Maaf, jumlah pinjaman melebihi limit.");
+        } else {
+            jumlahPeminjaman += uang;
+            System.out.println("Jumlah pinjaman saat ini : Rp" + jumlahPeminjaman);
+        }
+    }
+
+    public void angsur(int uang) {
+        if (uang > jumlahPeminjaman) {
+             System.out.println("Periksa jumlah uang yang Anda pinjam terlebih dahulu !");
+        } else {
+            jumlahPeminjaman -= uang;
+            System.out.println("Jumlah pinjaman saat ini : Rp" + jumlahPeminjaman);
+        }
+    }
+}
---
+++ jobsheet_03/tugas/studiKasus2/TestKoperasi_23.java
@@ -0,0 +1,23 @@
+package tugas.studiKasus2;
+
+public class TestKoperasi_23 {
+    public static void main(String[] args) {
+        //objek
+        Anggota_23 donny = new Anggota_23("111333444", "Donny", 5000000);
+
+        System.out.println("Nama Anggota    : " + donny.getNama());
+        System.out.println("Limit Pinjaman  : " + donny.getLimitPinjaman());
+
+        System.out.println("\nMeminjam uang 10.000.000...");
+        donny.pinjam(10000000);
+        
+        System.out.println("\nMeminjam uang 4.000.000...");
+        donny.pinjam(4000000);
+
+        System.out.println("\nMembayar angsuran 1.000.000...");
+        donny.angsur(1000000);
+
+        System.out.println("\nMembayar angsuran 3.000.000...");
+        donny.angsur(3000000);
+    }
+}
