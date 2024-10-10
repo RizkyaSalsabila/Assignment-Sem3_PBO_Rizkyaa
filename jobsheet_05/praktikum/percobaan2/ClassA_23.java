a7e790bfce4d3099388ab7e288ae644c24b2c920
RizkyaSalsabila <rzkysalsabila@gmail.com>
2024-10-10 09:39:23 +0700

    add + modif percobaan2

---
+++ jobsheet_05/praktikum/percobaan2/ClassA_23.java
@@ -0,0 +1,21 @@
+package percobaan2;
+
+public class ClassA_23 {
+    //atribut
+    protected int x;
+    protected int y;
+
+    //method
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public void getNilai() {
+        System.out.println("Nilai x : " + x);
+        System.out.println("Nilai y : " + y);
+    }
+}
---
+++ jobsheet_05/praktikum/percobaan2/Percobaan2_23.java
@@ -0,0 +1,15 @@
+package percobaan2;
+
+public class Percobaan2_23 {
+    public static void main(String[] args) {
+        //Buat objek hitung
+        ClassB_23 hitung = new ClassB_23();
+        hitung.setX(20);
+        hitung.setY(30);
+        hitung.setZ(5);
+
+        hitung.getNilai();
+        hitung.getNilaiZ();
+        hitung.getJumlah(); 
+    }
+}
