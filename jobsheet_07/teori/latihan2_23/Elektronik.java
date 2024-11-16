1caf108289bd4e925ef937aab69824962e0c4563
RizkyaSalsabila <rzkysalsabila@gmail.com>
2024-11-16 15:11:16 +0700

    add class parent_2

---
+++ jobsheet_07/teori/latihan2_23/Elektronik.java
@@ -0,0 +1,16 @@
+package latihan2_23;
+
+public class Elektronik {
+    //atribut
+    protected int voltase;
+
+    //konstruktor
+    public Elektronik(int voltase) {
+        this.voltase = voltase;
+    }
+
+    //method
+    public int getVoltase() {
+        return voltase;
+    } 
+}
