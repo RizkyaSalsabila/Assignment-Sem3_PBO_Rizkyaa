587ceb1bbf14bd39bc09128df24ec9ded0c166cf
RizkyaSalsabila <rzkysalsabila@gmail.com>
2024-11-26 22:26:45 +0700

    add_virtual method invocation

---
+++ jobsheet_07/praktikum/percobaan2_23/Employee.java
@@ -0,0 +1,11 @@
+package percobaan2_23;
+
+public class Employee {
+    //atribut
+    protected String name;
+
+    //method
+    public String getEmployeeInfo() {
+        return "Name : " + name;
+    }
+}
---
+++ jobsheet_07/praktikum/percobaan2_23/Payable.java
@@ -0,0 +1,6 @@
+package percobaan2_23;
+
+public interface Payable {
+    //method
+    public int getPaymentAmount();
+}
